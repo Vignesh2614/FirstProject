5717b190654cd754995f5843f7251b6f613bf3c1
Vignesh2614 <135516435+Vignesh2614@users.noreply.github.com>
2023-06-08 13:36:37 +0530

    Variable a changed to 15

--- add.java
+++ add.java
@@ -1,11 +1,7 @@
 class first
 {
     public static void main(String[] args) {
-<<<<<<< HEAD
-    int a =15; 
-=======
-    int a =22; 
->>>>>>> a02dd4dda778e199605bdcad52a1d7697f955c78
+    int a =15;
     int b=5;
     int sum = a*b;
     System.out.println("Sum of output:"+sum);
